a20e269784604ff498c26e61f789f6e77f7ede01
PatrickM-NAVDNA <PatrickM@avdna.com>
2022-01-28 11:25:49 -0600

    cleanup

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const fireBaseURL = 'https://job-listings-app-21d09-default-rtdb.firebaseio.com/';
 const jsonEXT = '.json';
-// const filters = document.getElementById("TagButtonID");
-// const filterContainer = document.getElementById()
-
-// console.log(filters);
 
 (function(){
     $.ajax({
@@ -15,7 +11,6 @@ const jsonEXT = '.json';
             Object.values(jobData).forEach(value => {
                 
                 let jobDataToArray = Object.values(value)
-                //console.log(jobDataToArray)
 
                 let companyName = jobDataToArray[0]
                 let contract = jobDataToArray[1]
@@ -35,7 +30,6 @@ const jsonEXT = '.json';
             })
 
             jobDataSortList.sort()
-            //console.log(jobDataSortList)
 
             Object.values(jobDataSortList).forEach(item => {
                 let timePosted = item[0]
@@ -117,77 +111,6 @@ const jsonEXT = '.json';
                 
             })
 
-            // Object.entries(jobData).forEach(([key, value]) => {
-
-            //     //console.log(value.company)
-
-                
-
-            //     let JobListingFeatured = `
-            //         <div class="jobListing"
-            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-            //         <div id="companyName"> ${value.company} </div>
-            //         <div id="featuredTag">FEATURED</div>
-            //         <div id="position"> ${value.position} </div>
-            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
-            //         <div class="break"></div>
-            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
-            //         </button>
-            //         </div>
-            //         </div>`
-            //     let JobListingNew = `
-            //         <div class="jobListing">
-            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-            //         <div id="companyName"> ${value.company} </div>
-            //         <div id="newTag">NEW!</div>
-            //         <div id="position"> ${value.position} </div>
-            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
-            //         <div class="break"></div>
-            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
-            //         </button>
-            //         </div>
-            //         </div>`
-            //     let JobListingNewFeatured = `
-            //         <div class="jobListing">
-            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-            //         <div id="companyName"> ${value.company} </div>
-            //         <div id="newTag">NEW!</div>
-            //         <div id="featuredTag">FEATURED</div>
-            //         <div id="position"> ${value.position} </div>
-            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
-            //         <div class="break"></div>
-            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
-            //         </button>
-            //         </div>
-            //         </div>`
-            //     let JobListingDefault = `
-            //         <div class="jobListing">
-            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-            //         <div id="companyName"> ${value.company} </div>
-            //         <div id="position"> ${value.position} </div>
-            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
-            //         <div class="break"></div>
-            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
-            //         </button>
-            //         </div>
-            //         </div>`
-                    
-
-            //     if(value.new == true && value.featured == true){
-            //         $('.mainContentContainer').append(JobListingNewFeatured)
-            //     } else if(value.new == true && value.featured == false){
-            //         $('.mainContentContainer').append(JobListingNew)
-            //     } else if(value.new == false && value.featured == true){
-            //         $('.mainContentContainer').append(JobListingFeatured)
-            //     } else if(value.new == false && value.featured == false){
-            //         $('.mainContentContainer').append(JobListingDefault)
-            //     }
-
-            // })
         },
         error: (error) =>{
             console.log('error');
@@ -196,15 +119,3 @@ const jsonEXT = '.json';
 })()
 
 let jobDataSortList = []
-
-
-// $('.requirementsTagsButton').on('click', function(event){
-
-//     event.stopPropagation();
-//     event.stopImmediatePropagation();
-
-//     let test = document.getElementById("tagButtonID").value
-
-
-//     console.log(test)
-// })
