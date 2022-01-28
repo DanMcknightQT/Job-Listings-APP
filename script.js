ef19df5634eecc11a6f20e08547fe01e73db3d5b
PatrickM-NAVDNA <PatrickM@avdna.com>
2022-01-28 11:18:51 -0600

    sort by date

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const jsonEXT = '.json';
             Object.values(jobData).forEach(value => {
                 
                 let jobDataToArray = Object.values(value)
-                console.log(jobDataToArray)
+                //console.log(jobDataToArray)
 
                 let companyName = jobDataToArray[0]
                 let contract = jobDataToArray[1]
@@ -35,79 +35,159 @@ const jsonEXT = '.json';
             })
 
             jobDataSortList.sort()
-            console.log(jobDataSortList)
+            //console.log(jobDataSortList)
 
-            Object.entries(jobData).forEach(([key, value]) => {
-
-                //console.log(value.company)
-
-                
+            Object.values(jobDataSortList).forEach(item => {
+                let timePosted = item[0]
+                let newT = item[1]
+                let featured = item[2]
+                let companyName = item[3]
+                let contract = item[4]
+                let id = item[5]
+                let level = item[6]
+                let location = item[7]
+                let logo = item[8]
+                let title = item[9]
+                let role = item[10]
+                let tags = item[11]
 
                 let JobListingFeatured = `
                     <div class="jobListing"
-                    <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-                    <div id="companyName"> ${value.company} </div>
+                    <div id="logoCircle"> <img id="logoImage" src="${logo}"> </img> </div>
+                    <div id="companyName"> ${companyName} </div>
                     <div id="featuredTag">FEATURED</div>
-                    <div id="position"> ${value.position} </div>
-                    <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+                    <div id="position"> ${title} </div>
+                    <div id="timeLocaleTags">${timePosted} • ${contract} • ${location}</div>
                     <div class="break"></div>
                     <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-                    ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+                    ${tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
                     </button>
                     </div>
                     </div>`
                 let JobListingNew = `
                     <div class="jobListing">
-                    <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-                    <div id="companyName"> ${value.company} </div>
+                    <div id="logoCircle"> <img id="logoImage" src="${logo}"> </img> </div>
+                    <div id="companyName"> ${companyName} </div>
                     <div id="newTag">NEW!</div>
-                    <div id="position"> ${value.position} </div>
-                    <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+                    <div id="position"> ${title} </div>
+                    <div id="timeLocaleTags">${timePosted} • ${contract} • ${location}</div>
                     <div class="break"></div>
                     <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-                    ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+                    ${tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
                     </button>
                     </div>
                     </div>`
                 let JobListingNewFeatured = `
                     <div class="jobListing">
-                    <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-                    <div id="companyName"> ${value.company} </div>
+                    <div id="logoCircle"> <img id="logoImage" src="${logo}"> </img> </div>
+                    <div id="companyName"> ${companyName} </div>
                     <div id="newTag">NEW!</div>
                     <div id="featuredTag">FEATURED</div>
-                    <div id="position"> ${value.position} </div>
-                    <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+                    <div id="position"> ${title} </div>
+                    <div id="timeLocaleTags">${timePosted} • ${contract} • ${location}</div>
                     <div class="break"></div>
                     <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-                    ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+                    ${tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
                     </button>
                     </div>
                     </div>`
                 let JobListingDefault = `
                     <div class="jobListing">
-                    <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
-                    <div id="companyName"> ${value.company} </div>
-                    <div id="position"> ${value.position} </div>
-                    <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+                    <div id="logoCircle"> <img id="logoImage" src="${logo}"> </img> </div>
+                    <div id="companyName"> ${companyName} </div>
+                    <div id="position"> ${title} </div>
+                    <div id="timeLocaleTags">${timePosted} • ${contract} • ${location}</div>
                     <div class="break"></div>
                     <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
-                    ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+                    ${tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
                     </button>
                     </div>
                     </div>`
                     
 
-                if(value.new == true && value.featured == true){
+                if(newT == true && featured == true){
                     $('.mainContentContainer').append(JobListingNewFeatured)
-                } else if(value.new == true && value.featured == false){
+                } else if(newT == true && featured == false){
                     $('.mainContentContainer').append(JobListingNew)
-                } else if(value.new == false && value.featured == true){
+                } else if(newT == false && featured == true){
                     $('.mainContentContainer').append(JobListingFeatured)
-                } else if(value.new == false && value.featured == false){
+                } else if(newT == false && featured == false){
                     $('.mainContentContainer').append(JobListingDefault)
                 }
-
+                
             })
+
+            // Object.entries(jobData).forEach(([key, value]) => {
+
+            //     //console.log(value.company)
+
+                
+
+            //     let JobListingFeatured = `
+            //         <div class="jobListing"
+            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
+            //         <div id="companyName"> ${value.company} </div>
+            //         <div id="featuredTag">FEATURED</div>
+            //         <div id="position"> ${value.position} </div>
+            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+            //         <div class="break"></div>
+            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
+            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+            //         </button>
+            //         </div>
+            //         </div>`
+            //     let JobListingNew = `
+            //         <div class="jobListing">
+            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
+            //         <div id="companyName"> ${value.company} </div>
+            //         <div id="newTag">NEW!</div>
+            //         <div id="position"> ${value.position} </div>
+            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+            //         <div class="break"></div>
+            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
+            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+            //         </button>
+            //         </div>
+            //         </div>`
+            //     let JobListingNewFeatured = `
+            //         <div class="jobListing">
+            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
+            //         <div id="companyName"> ${value.company} </div>
+            //         <div id="newTag">NEW!</div>
+            //         <div id="featuredTag">FEATURED</div>
+            //         <div id="position"> ${value.position} </div>
+            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+            //         <div class="break"></div>
+            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
+            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+            //         </button>
+            //         </div>
+            //         </div>`
+            //     let JobListingDefault = `
+            //         <div class="jobListing">
+            //         <div id="logoCircle"> <img id="logoImage" src="${value.logo}"> </img> </div>
+            //         <div id="companyName"> ${value.company} </div>
+            //         <div id="position"> ${value.position} </div>
+            //         <div id="timeLocaleTags">${value.postedAt} • ${value.contract} • ${value.location}</div>
+            //         <div class="break"></div>
+            //         <div class="requirementsTags"><button href="#" id="tagButtonID" class="requirementsTagsButton">
+            //         ${value.tags.join('</button> <button href="#" id="tagButtonID" class="requirementsTagsButton">')}
+            //         </button>
+            //         </div>
+            //         </div>`
+                    
+
+            //     if(value.new == true && value.featured == true){
+            //         $('.mainContentContainer').append(JobListingNewFeatured)
+            //     } else if(value.new == true && value.featured == false){
+            //         $('.mainContentContainer').append(JobListingNew)
+            //     } else if(value.new == false && value.featured == true){
+            //         $('.mainContentContainer').append(JobListingFeatured)
+            //     } else if(value.new == false && value.featured == false){
+            //         $('.mainContentContainer').append(JobListingDefault)
+            //     }
+
+            // })
         },
         error: (error) =>{
             console.log('error');
